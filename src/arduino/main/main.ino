e719e5bee7f38b7d4ab21787e9a9cc87efeb249e
Bruno Eduardo <bruno97br@gmail.com>
2020-04-21 06:12:55 -0300

    Arduino Serial Writer: 	Dumps the digital and analog ports states to UART output

---
+++ src/arduino/main/main.ino
@@ -0,0 +1,45 @@
+#define BAUDRATE 115200
+#define N_DIGITAL_PINS 14
+#define N_ANALOG_PINS 6
+
+#define digitalStateBufferSize (N_DIGITAL_PINS-8)/8+1
+
+void setup() {
+    Serial.begin(BAUDRATE);
+    while (!Serial) {;}
+    while (Serial.availableForWrite()>0){;}
+
+    // All digital pins as inputs
+    for (int i=0; i<=13; i++)
+        pinMode(i, INPUT);
+
+    // All analog pins as inputs
+    for (int i=A0; i<=A5; i++)
+        pinMode(i, INPUT);
+
+    // Sending number of pins to read
+    Serial.write(N_DIGITAL_PINS);
+    Serial.write(N_ANALOG_PINS);
+}
+
+void loop() {
+  
+    byte digitalBuffer;
+    char* analogBuffer = "0000";
+
+    // Dumping digital pins
+    for (int i=0, digitalBuffer=0; i<digitalStateBufferSize; i++)
+        digitalBuffer = 0;
+        for (int j=0+digitalStateBufferSize; j<8; j++){
+            digitalBuffer << 1;
+            digitalBuffer |= digitalRead(j);
+        }
+        Serial.write(digitalBuffer);
+
+    // Dumping analog pins as strings(comma-separated values)
+    for (int i=0; i<N_ANALOG_PINS; i++){
+        analogBuffer = itoa(analogRead(i), 4, 10);
+        Serial.write(analogBuffer);
+        Serial.write(',');
+    }
+}
