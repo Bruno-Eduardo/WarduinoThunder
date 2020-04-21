c1e4a2527c87646b0065a8c7da6bb7dababf0870
Bruno Eduardo <bruno97br@gmail.com>
2020-04-21 06:58:34 -0300

    Including Microcontroller class

--- src/python/main.py
+++ src/python/main.py
@@ -1,23 +1,17 @@
 import time
-from pynput.keyboard import Key, Controller
 
 import SerialController
+from Microcontroller import ArduinoUNO
 
 # Press and release space
 keyboard.press(Key.space)
 keyboard.release(Key.space)
 
-# Type a lower case A; this will work even if no key on the
-# physical keyboard is labelled 'A'
-keyboard.press('a')
-keyboard.release('a')
+microcontroller = ArduinoUNO()
 
-# Type two upper case As
-keyboard.press('A')
-keyboard.release('A')
-with keyboard.pressed(Key.shift):
-    keyboard.press('a')
-    keyboard.release('a')
+def main():
+    print(microcontroller.__dict__)
 
-# Type 'Hello World' using the shortcut type method
-keyboard.type('Hello World')
+
+if __name__ == '__main__':
+    main()
