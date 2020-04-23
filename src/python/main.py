4e7995a5187f55997d356ca63b908963275db75e
Bruno Eduardo <bruno97br@gmail.com>
2020-04-23 17:21:10 -0300

    Implementing calc_bytes_to_read method to Microcontroller class

--- src/python/main.py
+++ src/python/main.py
@@ -8,6 +8,8 @@ from Microcontroller import ArduinoUNO
 from pynput.keyboard import Key, Controller as KeyboardController
 from pynput.mouse import Button, Controller as MouseController
 
+
+
 keyboard = KeyboardController()
 mouse = MouseController()
 microcontroller = ArduinoUNO()
